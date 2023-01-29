0537bd5e0c72627b09305d539c35a9ce7cb1973e
Brian Li <me@brian14708.dev>
2023-01-29 17:59:42 +0800

    Add homepage

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://brian14708.dev",
-  integrations: [sitemap()],
+  integrations: [sitemap(), tailwind()],
 });
---
+++ tailwind.config.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  theme: {},
+  plugins: [require("@tailwindcss/typography")],
+};
